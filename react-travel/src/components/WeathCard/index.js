d02239cd03ef0e48e2b268818e30ebbbf3c603d4
Yunqi <857560476@qq.com>
2020-06-02 22:48:38 +0800

    新增规划功能

--- react-travel/src/components/WeathCard/index.js
+++ react-travel/src/components/WeathCard/index.js
@@ -1,47 +1,67 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import transWeekday from '../../utils/transWeekday'
+import transToPic from '../../utils/transToPic'
 import { Flex, Toast } from 'antd-mobile';
 import './index.scss'
 
 
 function WeatherCard({ city }) {
   const [weather, setWeather] = useState([])
+  const [url, setUrl] = useState('')
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
+    setLoading(true)
     axios.get('https://api.66mz8.com/api/weather.php', {
       params: {
-        location: '北京'
+        location: city
       }
     }).then(res => {
       console.log(res)
+      setUrl(transToPic('晴'))
       if (res.data && res.data.code == 200) {
-        setWeather(weather => [...weather, ...res.data.data])
+        setWeather(weather => [...res.data.data.slice(0,5)])
+        if(weather) {
+          setLoading(false)
+        }
       }else{
         Toast.fail(res.data.msg)
       }
     })
-  }, [])
+    
+  }, [city])
 
   const renderWeather = (weather) => {
     const flexChildren = []
-    weather.map((item, index) => flexChildren.push(<Flex.Item key={item.days}>
+    weather.map((item, index) => flexChildren.push(<Flex.Item key={item.days} style={{fontSize:'14px',fontFamily:'monospace'}}>
       {transWeekday(item.week).slice(0,3)}
+      <div style={{marginTop:'8px'}}>
+        <img src={transToPic(item.weather)} style={{width:'20px', height:'20px'}}></img>
+      </div>
+      <div style={{fontSize:'8px', marginTop:'8px'}}>{item.temperature}</div>
     </Flex.Item>))
     return flexChildren
   }
-  console.log(weather)
   return (
     <div className="weather-card">
-      <div className="city-weather">
-        <span>{weather[0] ? transWeekday(weather[0].week) : ''}</span>
-      </div>
-      <div className="info-weather">
-        <Flex>
-          {
-            renderWeather(weather)
-          }
-        </Flex>
-      </div>
+      {
+        loading ? <div>loading...</div> : 
+        <>
+          <div className="city-weather" style={{position:'relative'}}>
+              <span style={{fontSize:'16px'}}>{weather[0] ? transWeekday(weather[0].week) : ''}</span>
+              <span style={{position:'absolute', top:'3px', fontSize: 'inherit'}}>{weather[0] ? weather[0].days : ''}</span>
+              <span style={{position:'absolute',right:'10px', fontSize: 'inherit'}}>{city}</span>
+          </div>
+          <div className="spirt-line"></div>
+          <div className="info-weather">
+            <Flex>
+              {
+                renderWeather(weather)
+              }
+            </Flex>
+          </div> 
+        </>
+      }
     </div>
   )
 }
--- react-travel/src/pages/detail/index.js
+++ react-travel/src/pages/detail/index.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/NavBar'
 import WeatherCard from '../../components/WeathCard'
+import TravelPlan from '../../components/TravelPlan'
 import './index.scss'
 
 
 function Detail() {
+  const [ city, setCity ] = useState('杭州')
+
+  const getSelectCity = (city) => {
+    setCity(city)
+  }
 
   return (
-    <div className="detail-wrapper">
-      {/* <div className="bg-pic">
-        <img src="assets/imgs/bg_hangzhou.jpg" alt=""></img>
-      </div> */}
+    <div className="detail-wrapper" >
+      <div className="bg-pic">
+        <img src="assets/imgs/bg_hangZhou.jpg" alt=""></img>
+      </div>
       <Navbar title='杭州一日游' img='assets/imgs/avator.jpg'></Navbar>
-      <WeatherCard city='杭州'></WeatherCard>
+      <WeatherCard city={city}></WeatherCard>
+      <TravelPlan getSelectCity={getSelectCity}></TravelPlan>
     </div>
   )
 }
