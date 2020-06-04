c8e3bebbc280d78280951883c133b78f3d4e595b
Yunqi <857560476@qq.com>
2020-06-04 22:24:59 +0800

    更新

--- react-travel/src/components/WeathCard/index.js
+++ react-travel/src/components/WeathCard/index.js
@@ -14,7 +14,7 @@ function WeatherCard({ city }) {
   const [show, setShow] = useState(false)
   useEffect(() => {
     setLoading(true)
-    setShow(true)
+    // setShow(true)
     axios.get('https://api.66mz8.com/api/weather.php', {
       params: {
         location: city
@@ -47,20 +47,20 @@ function WeatherCard({ city }) {
     return flexChildren
   }
   return (
-    <CSSTransition
-      classNames="card"
-      timeout={20000}
-      in={show}>
+    // <CSSTransition
+    //   classNames="card"
+    //   timeout={200}
+    //   in={show}>
       <div className="weather-card" >
         {
           // loading ? <div>loading...</div> :
             <>
               <div className="city-weather" style={{ position: 'relative' }}>
                 <span style={{ fontSize: '16px' }}>{weather[0] ? transWeekday(weather[0].week) : ''}</span>
-                <span style={{ position: 'absolute', top: '3px', fontSize: 'inherit' }}>{weather[0] ? weather[0].days : ''}</span>
+                {/* <span style={{ position: 'absolute', top: '3px', fontSize: 'inherit' }}>{weather[0] ? weather[0].days : ''}</span> */}
                 <span style={{ position: 'absolute', right: '10px', fontSize: 'inherit' }}>{city}</span>
               </div>
-              <div className="spirt-line"></div>
+              {/* <div className="spirt-line"></div>  */}
               <div className="info-weather">
                 <Flex>
                   {
@@ -71,7 +71,7 @@ function WeatherCard({ city }) {
             </>
         }
       </div>
-    </CSSTransition>
+    // </CSSTransition>
   )
 }
 
